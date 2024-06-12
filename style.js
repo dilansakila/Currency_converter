ca7452a306f7f7f1dc48edaf020d02a4ce19a357
dilansakila <dilansakila99@gmail.com>
2024-06-12 21:08:25 +0530

    css and js update

--- style.js
+++ style.js
@@ -1,2 +1,3 @@
 var input_amount = document.getElementById("original-currency-amount");
 var from_currency = document.getElementById("fr_currency");
+var to_currency = document.getElementById("to_currency");
