f1886a0fa61736f6a2e4f77498011c07e359aab8
dilansakila <dilansakila99@gmail.com>
2024-06-12 20:15:37 +0530

    add js

--- style.js
+++ style.js
@@ -1,2 +1,2 @@
 var input_amount = document.getElementById("original-currency-amount");
-var from_currency = document.getElementById("from_currency");
+var from_currency = document.getElementById("fr_currency");
