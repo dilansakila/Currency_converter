2618109253ac5ba080ddc2bc77253b2a17898334
dilansakila <dilansakila99@gmail.com>
2024-06-12 21:40:17 +0530

    css and js update

--- style.js
+++ style.js
@@ -1,14 +1,14 @@
 var input_amount = document.getElementById("original-currency-amount");
-var from_currency = document.getElementById("fr_currency");
-var to_currency = document.gtElementById("to_currency");
+var from_currency = document.getElementById("from_currency");
+var to_currency = document.getElementById("to_currency");
 var exchange_rate = document.getElementById("exchange-rate");
-var exchange = document.getElementById("exchnge");
+var exchange = document.getElementById("exchange");
 var output_amount = document.getElementById("output-text");
 var output_from = document.getElementById("from");
-var outpt_to = document.getElemetById("to");
+var output_to = document.getElementById("to");
 
 
 exchange.addEventListener("click",()=>{
-    [from_currency.vlue, to_currency.value] = [to_currency.value, from_currency.value];
+    [from_currency.value, to_currency.value] = [to_currency.value, from_currency.value];
     calculate();
 })
