e055103a03c20f261686dc2b8778c5d469a42f38
dilansakila <dilansakila99@gmail.com>
2024-06-13 09:44:50 +0530

    updated style.js

--- style.js
+++ style.js
@@ -12,3 +12,25 @@ exchange.addEventListener("click",()=>{
     [from_currency.value, to_currency.value] = [to_currency.value, from_currency.value];
     calculate();
 })
+
+var to_amount = 0
+function calculate(){
+    const from_currency_value = from_currency.value;
+    const to_currency_value = to_currency.value;
+    
+    fetch()
+    .then(res => ))
+    .then(res => {
+        const rate = res.rates[to_currency_value];
+        exchange_rate.value = `${rate}`
+        output_from.innerText= `${input_amount.value} ${from_currency_value}`;
+        output_to.innerText = `${to_amount} ${to_currency_value}`;
+        output_amount.style.display="blck";
+    })
+}
+
+
+document.getElementById("exchangebutton").addEvent Listener("click",()=>{
+    calculate();
+});
+
